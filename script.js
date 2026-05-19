c6f0ee01c816f1bfef7e00389674ca845efc1aeb
Jaroslav Nesen <jara.nesen@gmail.com>
2026-05-19 13:15:02 +0200

    added clock, added dark mode

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 console.log("Hello, World!");
 console.log("Hello, World!");
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('Clock').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+function changeTheme() {
+  document.body.classList.toggle("dark-mode");
+}
